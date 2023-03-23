3ce061fa7f04a538916c3466a8e5ea28747479c2
ChangGe2211 <chang.ge.0224@gmail.com>
2023-03-23 12:42:47 +0100

    change dataset

--- src/data.py
+++ src/data.py
@@ -2,29 +2,16 @@ import torch as th
 import numpy as np
 from torch.utils.data import TensorDataset
 
-
-class Dataset(TensorDataset):
-    """
-    Model selection procedure for causal inference models
-
-    Args:
-     dataset: input file containing the features, treatments and outcomes
-     (add file type)
-    """
-
+class ihdpDataset(TensorDataset):
     def __init__(self, data_path):
-        ##Should I use a super init? I don't understand it's use in the assignements...
-        """
-           Takes a dataset as input and returns a parsed Torch tensor where each
-           row {X_i, T_i, Y_i} represents the feature vector, the treatment type
-           and the outcome for a particular person.
-        """
-
         data = np.load(data_path)
+        x_reshape = data['x'].reshape((data['x'].shape[0] * data['x'].shape[2], data['x'].shape[1]))
+        t_reshape = data['t'].reshape((data['t'].shape[0] * data['t'].shape[1], 1))
+        y_reshape = data['yf'].reshape((data['yf'].shape[0] * data['yf'].shape[1], 1))
+        # self.X = torch.tensor(x_reshape, dtype=torch.float32)
+        # self.T = torch.tensor(t_reshape, dtype=torch.float32)
+        # self.Y = torch.tensor(y_reshape, dtype=torch.float32)
+        super().__init__(th.from_numpy(x_reshape),
+                         th.from_numpy(t_reshape).int(),
+                         th.from_numpy(y_reshape))
 
-        # Each n row represents a person; the first column is the m dimensional float
-        # feature vector, the second column is the 0/1 treatment type, and the
-        # third column is the float outcome.
-        super().__init__(th.from_numpy(data['x']),
-                         th.from_numpy(data['t']).int(),
-                         th.from_numpy(data['yf']))
--- src/propensity_score.py
+++ src/propensity_score.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from typing import Tuple
+from sklearn.linear_model import LogisticRegression
 
 
 def propensityRegression(dataset: Tuple[torch.Tensor, torch.IntTensor, torch.Tensor]) -> torch.nn.Module:
@@ -15,4 +16,8 @@ def propensityRegression(dataset: Tuple[torch.Tensor, torch.IntTensor, torch.Ten
       A function that takes an m dimensional feature vector as input and
       estimates its propensity
     """
+
+    prop_model = LogisticRegression().fit(dataset.X, dataset.T)
+    prop_score = prop_model.predict proba(dataset.X)[:，1]
+    return prop_score
     # Call propensityScore to use the propensity for each input feature vector
