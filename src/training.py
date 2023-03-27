b620d7013b05a77487e9cf1a0edc18a78b17f363
lgkroes <lucas.kroes@hetnet.nl>
2023-03-27 12:49:28 +0200

    Add fit function wrapping training and evaluation with early stopping

--- src/training.py
+++ src/training.py
@@ -1,16 +1,46 @@
+import torch as th
+from torch.utils.data import DataLoader
+
+
+def fit(train_loader: DataLoader,
+        test_loader: DataLoader,
+        model: th.nn.Module,
+        optimizer: th.optim.Optimizer,
+        criterion: th.nn.Module,
+        early_stopping_patience: int):
+    min_val_loss = float('inf')
+    patience_count = 0
+    while patience_count < early_stopping_patience:
+        train_loss, accuracy = train(train_loader,
+                                     model,
+                                     optimizer,
+                                     criterion)
+        print(f"train accuracy: {accuracy}")
+        print(f"train loss: {train_loss}")
+        val_loss, accuracy = test(test_loader,
+                                  model,
+                                  criterion)
+        print(f"val accuracy: {accuracy}")
+        print(f"val loss: {val_loss}")
+        if val_loss > min_val_loss:
+            early_stopping_patience += 1
+
+        min_val_loss = min(min_val_loss, val_loss)
+
+
 # Copy and paste from the assignements --> check this
 # Is this where we have to input equation 5?
-def train(train_loader, net, optimizer, criterion):
+def train(train_loader, model, optimizer, criterion):
     """
     Trains network for one epoch in batches.
 
     Args:
         train_loader: Data loader for training set.
-        net: Neural network model.
+        model: Neural network model.
         optimizer: Optimizer (e.g. SGD).
         criterion: Loss function (e.g. cross-entropy loss).
     """
-
+    model.train()
     avg_loss = 0
     correct = 0
     total = 0
@@ -24,7 +54,7 @@ def train(train_loader, net, optimizer, criterion):
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        outputs = net(inputs)
+        outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -38,16 +68,16 @@ def train(train_loader, net, optimizer, criterion):
     return avg_loss / len(train_loader), 100 * correct / total
 
 
-def test(test_loader, net, criterion):
+def test(test_loader, model, criterion):
     """
     Evaluates network in batches.
 
     Args:
         test_loader: Data loader for test set.
-        net: Neural network model.
+        model: Neural network model.
         criterion: Loss function (e.g. cross-entropy loss).
     """
-
+    model.eval()
     avg_loss = 0
     correct = 0
     total = 0
@@ -60,7 +90,7 @@ def test(test_loader, net, criterion):
             inputs, labels = data
 
             # forward pass
-            outputs = net(inputs)
+            outputs = model(inputs)
             loss = criterion(outputs, labels)
 
             # keep track of loss and accuracy
